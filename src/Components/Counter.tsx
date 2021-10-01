6d042369270bdc3ac44923c16ba29dad7ea624eb
harryli0088 <harryli0088@gmail.com>
2021-10-01 02:24:21 -0400

    updated text

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -13,7 +13,7 @@ const Counter = () => {
       <br/>
       <div><b>Recoil Counter:</b> {count}</div>
       <div><button onClick={() => setCount(count + 1)}>Increment</button></div>
-      <p>This app uses <BlankAnchor href="https://recoiljs.org/">Recoil</BlankAnchor> as a state manager for React. The global state is persisted across pages with Gatsby's via the <BlankAnchor href="https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapPageElement">wrapPageElement API in the gatsby-browser.js file</BlankAnchor>. If you increment the count then change pages, the count will persist.</p>
+      <p>This app uses <BlankAnchor href="https://recoiljs.org/">Recoil</BlankAnchor> as a state manager for React. The global state is persisted across pages with the <BlankAnchor href="https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/#wrapPageElement">wrapPageElement API in the gatsby-browser.js and gatsby-ssr.js files</BlankAnchor>. If you increment the count then change pages, the count will persist.</p>
       <br/>
     </div>
   )
